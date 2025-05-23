7062e28e666f0f314837ad2256cdaebf2b92b698
LB <loganbek@gmail.com>
2025-05-22 18:28:11 -0700

    Integrate LinPEAS tool

--- cai/tools/__init__.py
+++ cai/tools/__init__.py
@@ -6,7 +6,8 @@ from .pillaging import (
     run_crackmapexec_and_parse
 )
 from .reconnaissance import run_nmap_scan
-from .web import run_nuclei_scan, run_nikto_scan # Added run_nikto_scan
+from .web import run_nuclei_scan, run_nikto_scan
+from .privilege_scalation import run_linpeas # Added import
 
 __all__ = [
     # ... other existing exports ...
@@ -17,5 +18,6 @@ __all__ = [
     "run_crackmapexec_and_parse",
     "run_nmap_scan",
     "run_nuclei_scan",
-    "run_nikto_scan", # Added to __all__
+    "run_nikto_scan",
+    "run_linpeas", # Added to __all__
 ]
